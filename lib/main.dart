e5c71ea6ce69daeb7fe65707a1299d1d18ef6b42
Mateus <teus.alves.sp@gmail.com>
2024-04-25 14:36:02 -0300

    upload - pages organize

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 // PAGES
-// import 'package:unaspbank/pages/create_user.dart';
-// import 'package:unaspbank/pages/sucess_signup.dart';
-// import 'package:unaspbank/pages/login_page.dart';
-// import 'package:unaspbank/pages/financing.dart';
-// import 'package:unaspbank/pages/image_verify.dart';
-// import 'package:unaspbank/pages/document_verify.dart';
-// import 'package:unaspbank/pages/send_doc_sucess.dart';
-// import 'package:unaspbank/pages/monthly_income.dart';
-//import 'package:unaspbank/pages/register_completed.dart';
+// import 'package:unaspbank/pages/signup/create_user.dart';
+// import 'package:unaspbank/pages/signup/sucess_signup.dart';
+// import 'package:unaspbank/pages/signup/login_page.dart';
+// import 'package:unaspbank/pages/financing/financing.dart';
+// import 'package:unaspbank/pages/signup/image_verify.dart';
+// import 'package:unaspbank/pages/signup/document_verify.dart';
+// import 'package:unaspbank/pages/signup/send_doc_sucess.dart';
+// import 'package:unaspbank/pages/signup/monthly_income.dart';
+// import 'package:unaspbank/pages/signup/register_completed.dart';
+import 'package:unaspbank/pages/financing/Installment_financing.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: RegisterCompleted(),
+      home: InstallmentFinancing(),
       debugShowCheckedModeBanner: false,
     );
   }
